17bfa2fdceb2597767ecbc9e18b69e6fc0e07604
cyril279 <cyril.x9@gmail.com>
2016-03-28 15:47:11 -0400

    Add filtering for caf-msm8960 audio variant ref: https://github.com/omnirom/android_hardware_qcom_audio-caf-msm8974/commit/d9b82d947c10826c2035abd87ffa46ee7ebe4600

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf-msm8960)
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -8,4 +9,5 @@ else
 include $(MY_LOCAL_PATH)/hal/Android.mk
 endif
 
+endif
 endif
